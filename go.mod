03a65c00f2c2d0cdce74c46447a3f38c2b16ef0c
lcw729 <lcw729729@gmail.com>
2022-09-27 18:26:45 +0900

    n

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module Hybrid_LCW/git_test
 
 go 1.18
+
+require github.com/KETI-Hybrid/test2 v0.0.0-20220927091102-165d3150ed4c
--- test.go
+++ test.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-
-"fmt"
+	test2 "github.com/KETI-Hybrid/test2"
 )
 
 func main() {
-	
+	test2.Print()
 }
